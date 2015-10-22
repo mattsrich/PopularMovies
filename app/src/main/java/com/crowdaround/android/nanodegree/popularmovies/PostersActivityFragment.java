0312b7b0d6812c3af7872d2568af717c333c40c0
Matt Richardson <mattsrich@gmail.com>
2015-10-22 13:12:26 -0700

    Initial commit. Boilerplate new project with keys.xml added.

---
+++ app/src/main/java/com/crowdaround/android/nanodegree/popularmovies/PostersActivityFragment.java
@@ -0,0 +1,22 @@
+package com.crowdaround.android.nanodegree.popularmovies;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class PostersActivityFragment extends Fragment {
+
+    public PostersActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_posters, container, false);
+    }
+}
